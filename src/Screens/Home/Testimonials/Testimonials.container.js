8a7e7d8c5c61e1a72d347c7e0578f98daebc6f1d
Ricky J <rickyjulpiter14@gmail.com>
2022-03-19 00:01:56 +0700

    Deploy to Heroku

--- src/Screens/Home/Testimonials/Testimonials.container.js
+++ src/Screens/Home/Testimonials/Testimonials.container.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { ContainerStyled } from '../OurPrograms/MainPrograms.styled';
-import { useTranslation } from 'react-i18next';
 
 const TestimonialsContainer = () => {
-  const { t } = useTranslation();
   return (
     <ContainerStyled>
       {/*<SectionTitle>{t('testimonials')}</SectionTitle>*/}
