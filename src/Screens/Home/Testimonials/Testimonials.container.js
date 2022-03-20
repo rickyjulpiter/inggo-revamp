987bc5ad52e5fa75251c2eef3c49064f69c50134
Ricky J <rickyjulpiter14@gmail.com>
2022-03-20 18:53:37 +0700

    Make Testimonials to slider

--- src/Screens/Home/Testimonials/Testimonials.container.js
+++ src/Screens/Home/Testimonials/Testimonials.container.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Col, Row } from 'react-bootstrap';
+import { Carousel, Col, Row } from 'react-bootstrap';
 import {
   CardTestimonials,
   ContainerStyled,
@@ -43,114 +43,143 @@ const TestimonialsContainer = () => {
   return (
     <ContainerStyled>
       <SectionTitle>{t('testimonials')}</SectionTitle>
-      <SectionSectionTitle>A Word From Our Customer</SectionSectionTitle>
-      <Row className="mt-5">
-        <Col md={4} xl={4} className="mb-4">
-          <CenterDiv>
-            <ImgStyled
-              className="position-absolute index-1 center"
-              src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
-              alt="a"
-            />
-          </CenterDiv>
-          <CardTestimonials style={{ marginTop: '3..3vw' }}>
-            <TestimonialsDescription>
-              <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo in,
-              vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus
-              est."
-            </TestimonialsDescription>
-            <LineStyled />
-            <TestimonialsPerson>
-              <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
-              <PersonStyled> Ricky J</PersonStyled>
-            </TestimonialsPerson>
-          </CardTestimonials>
-        </Col>
-        <Col md={4} xl={4} className="mb-4">
-          <CenterDiv>
-            <ImgStyled
-              className="position-absolute index-1 center"
-              src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
-              alt="a"
-            />
-          </CenterDiv>
-          <CardTestimonials>
-            <TestimonialsDescription>
-              <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo in,
-              vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus
-              est."
-            </TestimonialsDescription>
-            <LineStyled />
-            <TestimonialsPerson>
-              <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
-              <PersonStyled> Ricky J</PersonStyled>
-            </TestimonialsPerson>
-          </CardTestimonials>
-        </Col>
-        <Col md={4} xl={4} className="mb-4">
-          <CenterDiv>
-            <ImgStyled
-              className="position-absolute index-1 center"
-              src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
-              alt="a"
-            />
-          </CenterDiv>
-          <CardTestimonials>
-            <TestimonialsDescription>
-              <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo in,
-              vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus
-              est."
-            </TestimonialsDescription>
-            <LineStyled />
-            <TestimonialsPerson>
-              <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
-              <PersonStyled> Ricky J</PersonStyled>
-            </TestimonialsPerson>
-          </CardTestimonials>
-        </Col>
-        <Col md={4} xl={4} className="mb-4">
-          <CenterDiv>
-            <ImgStyled
-              className="position-absolute index-1 center"
-              src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
-              alt="a"
-            />
-          </CenterDiv>
-          <CardTestimonials>
-            <TestimonialsDescription>
-              <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo in,
-              vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus
-              est."
-            </TestimonialsDescription>
-            <LineStyled />
-            <TestimonialsPerson>
-              <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
-              <PersonStyled> Ricky J</PersonStyled>
-            </TestimonialsPerson>
-          </CardTestimonials>
-        </Col>
-        <Col md={4} xl={4} className="mb-4">
-          <CenterDiv>
-            <ImgStyled
-              className="position-absolute index-1 center"
-              src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
-              alt="a"
-            />
-          </CenterDiv>
-          <CardTestimonials>
-            <TestimonialsDescription>
-              <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo in,
-              vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus
-              est."
-            </TestimonialsDescription>
-            <LineStyled />
-            <TestimonialsPerson>
-              <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
-              <PersonStyled> Ricky J</PersonStyled>
-            </TestimonialsPerson>
-          </CardTestimonials>
-        </Col>
-      </Row>
+      <SectionSectionTitle className="mb-5">A Word From Our Customer</SectionSectionTitle>
+      <Carousel fade variant="dark">
+        <Carousel.Item interval={1000}>
+          <Row>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials style={{ marginTop: '3..3vw' }}>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+          </Row>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <Row>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/AlaaQaharuddin.7db783ed.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials style={{ marginTop: '3..3vw' }}>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/AlaaQaharuddin.7db783ed.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+            <Col md={4} xl={4} className="mb-4">
+              <CenterDiv>
+                <ImgStyled
+                  className="position-absolute index-1 center"
+                  src="https://www.kobieducation.com/static/media/RachmadiantiSukmaHanifa.4cfb28bb.png"
+                  alt="a"
+                />
+              </CenterDiv>
+              <CardTestimonials>
+                <TestimonialsDescription>
+                  <i className="bi bi-chat-right-quote" /> "Aenean tortor est, vulputate quis leo
+                  in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius
+                  finibus est."
+                </TestimonialsDescription>
+                <LineStyled />
+                <TestimonialsPerson>
+                  <AvatarImg src={Avatar} className="img-fluid" alt="client-logo" />
+                  <PersonStyled> Ricky J</PersonStyled>
+                </TestimonialsPerson>
+              </CardTestimonials>
+            </Col>
+          </Row>
+        </Carousel.Item>
+      </Carousel>
     </ContainerStyled>
   );
 };
