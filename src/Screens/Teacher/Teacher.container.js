6418cc659be333c44b938500d9998872d1fcdc50
Ricky J <rickyjulpiter14@gmail.com>
2022-05-08 18:21:28 +0700

    Add another section on our team section

--- src/Screens/Teacher/Teacher.container.js
+++ src/Screens/Teacher/Teacher.container.js
@@ -13,120 +13,130 @@ import DessyImages from '../../Assets/Images/teachers/Dessy.jpg';
 import KomarImages from '../../Assets/Images/teachers/Komar.jpg';
 import AndinImages from '../../Assets/Images/teachers/Andin.jpeg';
 import MedImages from '../../Assets/Images/teachers/Med.jpeg';
+import WhyChooseUsContainer from '../Home/WhyChooseUs/WhyChooseUs.container';
+import TestimonialsComponentContainer from '../Home/TestimonialsComponent/TestimonialsComponent.container';
+import OurClientsContainer from '../Home/OurClients/OurClients.container';
+import ContactContainer from '../Home/Contact/Contact.container';
 
 const TeacherContainer = () => {
   const { t } = useTranslation();
   return (
-    <ContainerStyled>
-      <Title>{t('menuOurTeam')}</Title>
-      <Row style={{ marginTop: '3vw' }}>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={PutriImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Putri</TeacherCardTitle>
-              <TeacherCardPosition>Founder</TeacherCardPosition>
-              <TeacherCardSubject>MA TESOL - King’s College London</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-      </Row>
-      <Row style={{ marginTop: '3vw' }}>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={DisaImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Disa</TeacherCardTitle>
-              <TeacherCardSubject>Applied Linguistics - New Zealand</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={NiaImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Nia</TeacherCardTitle>
-              <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={NisaImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Nisa</TeacherCardTitle>
-              <TeacherCardSubject>TESOL - Australia</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={RiskaImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Riska</TeacherCardTitle>
-              <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-      </Row>
+    <>
+      <ContainerStyled>
+        <Title>{t('menuOurTeam')}</Title>
+        <Row style={{ marginTop: '3vw' }}>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={PutriImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Putri</TeacherCardTitle>
+                <TeacherCardPosition>Founder</TeacherCardPosition>
+                <TeacherCardSubject>MA TESOL - King’s College London</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: '3vw' }}>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={DisaImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Disa</TeacherCardTitle>
+                <TeacherCardSubject>Applied Linguistics - New Zealand</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={NiaImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Nia</TeacherCardTitle>
+                <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={NisaImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Nisa</TeacherCardTitle>
+                <TeacherCardSubject>TESOL - Australia</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={RiskaImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Riska</TeacherCardTitle>
+                <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+        </Row>
 
-      <Row style={{ marginTop: '3vw' }}>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={DessyImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Dessy</TeacherCardTitle>
-              <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={KomarImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Mr. Komar</TeacherCardTitle>
-              <TeacherCardSubject>Applied Linguistics - Australia</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={AndinImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Andin</TeacherCardTitle>
-              <TeacherCardSubject>Journalism - Qatar</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-        <Col xl={3} md={3} sm={12} className="mx-auto">
-          <TeacherCard>
-            <center>
-              <TeacherCardImage variant="top" src={MedImages} />
-            </center>
-            <Card.Body>
-              <TeacherCardTitle>Ms. Med</TeacherCardTitle>
-              <TeacherCardSubject>English Studies - Indonesia</TeacherCardSubject>
-            </Card.Body>
-          </TeacherCard>
-        </Col>
-      </Row>
-    </ContainerStyled>
+        <Row style={{ marginTop: '3vw' }}>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={DessyImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Dessy</TeacherCardTitle>
+                <TeacherCardSubject>TESOL - UK</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={KomarImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Mr. Komar</TeacherCardTitle>
+                <TeacherCardSubject>Applied Linguistics - Australia</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={AndinImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Andin</TeacherCardTitle>
+                <TeacherCardSubject>Journalism - Qatar</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+          <Col xl={3} md={3} sm={12} className="mx-auto">
+            <TeacherCard>
+              <center>
+                <TeacherCardImage variant="top" src={MedImages} />
+              </center>
+              <Card.Body>
+                <TeacherCardTitle>Ms. Med</TeacherCardTitle>
+                <TeacherCardSubject>English Studies - Indonesia</TeacherCardSubject>
+              </Card.Body>
+            </TeacherCard>
+          </Col>
+        </Row>
+      </ContainerStyled>
+      <WhyChooseUsContainer />
+      <TestimonialsComponentContainer />
+      <OurClientsContainer />
+      <ContactContainer />
+    </>
   );
 };
 
