57a1fb13642114a24e0c57421469f724622e467b
Ricky J <rickyjulpiter14@gmail.com>
2022-07-21 10:47:52 +0700

    Save redux to localstorage

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,24 @@
+/* eslint-disable */
 import { configureStore } from '@reduxjs/toolkit';
 import handleAnswer from './handleAnswer';
 
+//MIDDLEWARE
+const localStorageMiddleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem('applicationState', JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  if (localStorage.getItem('applicationState') !== null) {
+    return JSON.parse(localStorage.getItem('applicationState')); // re-hydrate the store
+  }
+};
+
 export default configureStore({
-  reducer: { answer: handleAnswer }
+  reducer: { answer: handleAnswer },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware)
 });
